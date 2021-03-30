11d52d63d9abeedc7afc7feb2140025e334f077c
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-03-30 18:34:25 +0600

    database added and nodemon fixed

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const cors = require('cors')
 const dotenv = require('dotenv')
 const env = dotenv.config().parsed
 const app = express()
-
+const port = 5713
 app.use(cors())
 app.get('/', (req, res) => {
     res.send('Hello Emma Jane')
 })
 
-app.listen(env.PORT, () => {
+const MongoClient = require('mongodb').MongoClient;
+
+// Connection URL
+const url = `mongodb+srv://${env.DB_USER}:${env.DB_PASS}@cluster0.xzynl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, client) {
+    const products = client.db("emmajanestore").collection("products");
+    console.log('database connected succesfully')
+});
+
+
+app.listen(env.PORT || port, () => {
     console.log('server is running ..')
 })
